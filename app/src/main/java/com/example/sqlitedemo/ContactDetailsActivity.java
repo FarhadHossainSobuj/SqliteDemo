0fb218b99ecf98aeb65d26b400fcc57945fe2c7d
Farhad Hossain <32379259+FarhadHossainSobuj@users.noreply.github.com>
2020-01-14 08:10:42 +0600

    Searching added

--- app/src/main/java/com/example/sqlitedemo/ContactDetailsActivity.java
+++ app/src/main/java/com/example/sqlitedemo/ContactDetailsActivity.java
@@ -2,13 +2,88 @@ package com.example.sqlitedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ContactDetailsActivity extends AppCompatActivity {
+
+    private TextView tvId, tvName, tvNumber, tvEmail;
+    private Button btnUpdate;
+    private ImageView imgCall, imgSendMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_details);
+
+        //tvId = findViewById(R.id.tvId);
+        tvName = findViewById(R.id.tvName);
+        tvNumber = findViewById(R.id.tvNumber);
+        tvEmail = findViewById(R.id.tvEmail);
+        btnUpdate = findViewById(R.id.btnEdit);
+        imgCall = findViewById(R.id.imgCall);
+        imgSendMessage = findViewById(R.id.imgMessage);
+
+        final String id = getIntent().getStringExtra("id");
+        String name = "";
+        String number = "";
+        String email = "";
+
+        final DatabaseHelper dbHelper = new DatabaseHelper(this);
+        Cursor data = dbHelper.getSingleData(id);
+
+        while (data.moveToNext()) {
+
+            name = data.getString(1);
+            number = data.getString(2);
+            email = data.getString(3);
+        }
+
+        //tvId.setText("" + id);
+        tvName.setText("" + name);
+        tvNumber.setText("" + number);
+        tvEmail.setText("" + email);
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ShowDetailsActivity.class);
+                intent.putExtra("id", id);
+                startActivity(intent);
+            }
+        });
+
+        final String finalNumber = number;
+        imgCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + finalNumber));
+                if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    Activity#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for Activity#requestPermissions for more details.
+                    return;
+                }
+                startActivity(callIntent);
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/sqlitedemo/DisplayActivity.java
+++ app/src/main/java/com/example/sqlitedemo/DisplayActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -21,6 +24,8 @@ public class DisplayActivity extends AppCompatActivity {
     private ListView listView;
     private DatabaseHelper db;
     private List<String> listData;
+    private EditText edtSearch;
+    ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +36,35 @@ public class DisplayActivity extends AppCompatActivity {
         listData = new ArrayList<>();
         listView = findViewById(R.id.listView);
 
+        edtSearch = findViewById(R.id.edtSearch);
+
         getAllData();
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listData);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listData);
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplicationContext(), ShowDetailsActivity.class);
+                Intent intent = new Intent(getApplicationContext(), ContactDetailsActivity.class);
                 intent.putExtra("id", ""+(position+1));
                 startActivity(intent);
+            }
+        });
+
+        edtSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                DisplayActivity.this.adapter.getFilter().filter(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
 
